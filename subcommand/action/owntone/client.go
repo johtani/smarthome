a9e767c39f5db11a976b8c7e644f7f342e9f1149
Jun Ohtani <johtani@gmail.com>
2023-04-08 01:38:05 +0900

    キューが空の時の挙動を追加

--- subcommand/action/owntone/client.go
+++ subcommand/action/owntone/client.go
@@ -1,6 +1,7 @@
 package owntone
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -88,4 +89,85 @@ func (c Client) SetVolume(volume int) error {
 		return fmt.Errorf("something wrong... status code is %d. %v", res.StatusCode, res.Header)
 	}
 	return nil
+}
+
+func (c Client) GetPlaylists() ([]string, error) {
+	type Item struct {
+		Uri string `json:"uri"`
+	}
+	type Playlists struct {
+		Items []Item `json:"items"`
+		Total int    `json:"total"`
+	}
+	req, err := http.NewRequest(http.MethodGet, c.buildUrl("api/library/playlists"), nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("something wrong... status code is %d. %v", res.StatusCode, res.Header)
+	}
+	var p Playlists
+	if err := json.NewDecoder(res.Body).Decode(&p); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %v", err)
+	}
+	var lists []string
+	for _, item := range p.Items {
+		lists = append(lists, item.Uri)
+	}
+	return lists, nil
+}
+
+func (c Client) AddItem2Queue(uri string) error {
+	req, err := http.NewRequest(http.MethodPost, c.buildUrl("api/queue/items/add"), nil)
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	q.Set("uris", uri)
+	req.URL.RawQuery = q.Encode()
+	res, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("something wrong... status code is %d. %v", res.StatusCode, res.Header)
+	}
+	return nil
+}
+
+type PlayerStatus struct {
+	State          string `json:"state"`
+	Repeat         string `json:"repeat"`
+	Consume        bool   `json:"consume"`
+	Shuffle        bool   `json:"shuffle"`
+	Volume         int    `json:"volume"`
+	ItemID         int    `json:"item_id"`
+	ItemLengthMS   int    `json:"item_length_ms"`
+	ItemProgressMS int    `json:"item_progress_ms"`
+}
+
+func (c Client) GetPlayerStatus() (*PlayerStatus, error) {
+	req, err := http.NewRequest(http.MethodGet, c.buildUrl("api/player"), nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("something wrong... status code is %d. %v", res.StatusCode, res.Header)
+	}
+	var p PlayerStatus
+	if err := json.NewDecoder(res.Body).Decode(&p); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %v", err)
+	}
+	return &p, nil
 }
--- subcommand/action/owntone/play.go
+++ subcommand/action/owntone/play.go
@@ -1,13 +1,43 @@
 package owntone
 
+import (
+	"math/rand"
+	"time"
+)
+
 type PlayAction struct {
 	name string
 	c    Client
 }
 
 func (a PlayAction) Run() error {
-	err := a.c.Play()
+	status, err := a.c.GetPlayerStatus()
 	if err != nil {
+		return err
+	}
+	if status.ItemID == 0 {
+		println("queue is empty, so playing a randomly selected playlist")
+		playlists, err := a.c.GetPlaylists()
+		if err != nil {
+			println("error in GetPlaylists")
+			return err
+		}
+		if len(playlists) > 0 {
+			rand.Seed(time.Now().UnixNano())
+			index := rand.Intn(len(playlists))
+			target := playlists[index]
+			err := a.c.AddItem2Queue(target)
+			if err != nil {
+				println("error in AddItem2Queue")
+				return err
+			}
+		} else {
+			println("playlists is empty")
+		}
+	}
+	err = a.c.Play()
+	if err != nil {
+		println("error in Play")
 		return err
 	}
 	println("owntone play action succeeded.")
